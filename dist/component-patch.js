34422cd4c78401d6c5ab258f7ff038c7f61f758f
Tommy Maloney <tmaloney@5inline.com>
2015-06-14 13:01:49 -0500

    Fix for consistent versioning for Bower.

--- dist/component-patch.js
+++ dist/component-patch.js
@@ -1,6 +1,6 @@
 /**
  * Component monkey patch for Angular 1.3+
- * @version  1.0.0
+ * @version  1.0.1
  * {@link  https://github.com/5inline/angular-component-patch}
  * @license  MIT License 
  */
