016986952c391c2d1681aa7d0ee3a503bd239bed
Tommy Maloney <tmaloney@5inline.com>
2015-07-28 12:03:37 -0500

    Fixed for multiple events in a controller cleanup on destroy. Fixes potential memory leaks.

--- dist/component-patch.js
+++ dist/component-patch.js
@@ -1,6 +1,6 @@
 /**
  * Component monkey patch for Angular 1.3+
- * @version  1.0.3
+ * @version  1.0.4
  * {@link  https://github.com/5inline/angular-component-patch}
  * @license  MIT License 
  */
--- dist/flux-component-patch.js
+++ dist/flux-component-patch.js
@@ -1,6 +1,6 @@
 /**
  * Component monkey patch for Angular 1.3+
- * @version  1.0.3
+ * @version  1.0.4
  * {@link  https://github.com/5inline/angular-component-patch}
  * @license  MIT License 
  */
@@ -36,6 +36,10 @@ angular.module = function ()
 				this.storeExport = storeExport;
 				this.eventName = eventName;
 				this.callback = callback;
+				if( !this.events ) {
+					this.events = [];
+				}
+				this.events.push({eventName:eventName,callback:callback});
 			}
 
 			function defaultLink ($scope, $elem, $attr, controller)
@@ -44,9 +48,12 @@ angular.module = function ()
 				{
 					if( !controller.storeExport ) return;
 					var store = flux.getStore(controller.storeExport);
-					var removeMethod = controller.eventName === '*' ? 'offAny' : 'off';
-					var args = controller.eventName === '*' ? [controller.callback] : [controller.eventName, controller.callback];
-					store[removeMethod].apply(store, args);
+					controller.events.forEach( function (event)
+					{
+						var removeMethod = event.eventName === '*' ? 'offAny' : 'off';
+						var args = event.eventName === '*' ? [event.callback] : [event.eventName, event.callback];
+						store[removeMethod].apply(store, args);
+					});
 				});
 			}
 
